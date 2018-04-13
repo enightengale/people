97c4bc4cc5efa1df69cf6be9513ff09f42cfbb42
Elijah Nightengale <elijah.charles.smile@gmail.com>
2018-04-13 10:12:20 -0400

    add comments to help describe what's going on

--- lib/people.rb
+++ lib/people.rb
@@ -3,20 +3,27 @@ require("pry")
 class Person
   @@people = []
 
+  # we want to be able to read each Person's id so that we can
+  # use that id to find that Person and all their parameters
   attr_reader :id
 
+  # pass in these parameters to help define your Person!!
   def initialize(firstName, lastName, hobby, location)
     @firstName = firstName
     @lastName = lastName
     @hobby = hobby
     @location = location
+    # every time a new Person is created we add one to id so
+    # that each Person has a unique id
     @id = @@people.length + 1
   end
 
+  # show all of the Person's that have been pushed into @@people
   def self.all()
     @@people
   end
 
+  # push "this" Person into the array @people
   def save()
     @@people.push(self)
   end
